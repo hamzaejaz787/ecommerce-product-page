8b330e95f31da7b5eb72b322587e8127f8f20b5b
hamzaejaz787 <hejaz389@gmail.com>
2023-05-24 17:12:09 +0500

    ui changes, added new content

--- src/containers/Product/Product.jsx
+++ src/containers/Product/Product.jsx
@@ -1,18 +1,26 @@
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useState } from "react";
 
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 import "./product.css";
 
 const Product = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
   return (
     <section className="product">
       <strong className="product-logo">Sneaker Company</strong>
 
-      <h1 className="product-title">
-        Fall Limited Edition <br />
-        Sneakers
-      </h1>
+      <h1 className="product-title">Fall Limited Edition Sneakers</h1>
       <p className="product-description">
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everything the
@@ -26,13 +34,20 @@ const Product = () => {
 
       <div className="product__checkout">
         <div className="product__checkout-quantity">
-          <FaMinus className="quantity-btn" />
-          <span>0</span>
-          <FaPlus className="quantity-btn" />
+          <button>
+            <FaMinus className="quantity-btn" onClick={decrement} />
+          </button>
+
+          <span>{count}</span>
+
+          <button>
+            <FaPlus className="quantity-btn" onClick={increment} />
+          </button>
         </div>
 
         <button className="product__checkout-cta">
-          <AiOutlineShoppingCart className="cta-icon" size={22} /> Add to cart
+          <AiOutlineShoppingCart className="cta-icon" size={22} />
+          Add to cart
         </button>
       </div>
     </section>
--- src/containers/ProductImage/ProductImage.jsx
+++ src/containers/ProductImage/ProductImage.jsx
@@ -4,12 +4,21 @@ import productImageThumbnail2 from "../../assets/image-product-2-thumbnail.jpg";
 import productImageThumbnail3 from "../../assets/image-product-3-thumbnail.jpg";
 import productImageThumbnail4 from "../../assets/image-product-4-thumbnail.jpg";
 
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 import "./product-image.css";
 
 const ProductImage = () => {
   return (
     <div className="product__image">
-      <img src={productImage1} alt="" className="product__image-main" />
+      <div className="main__image-container">
+        <img src={productImage1} alt="" className="product__image-main" />
+        <button className="image-nav previous">
+          <GrFormPrevious size={27} />
+        </button>
+        <button className="image-nav next">
+          <GrFormNext size={27} />
+        </button>
+      </div>
 
       <div className="product__image-thumbnails">
         <div className="product__image-thumbnail">
