423b7fb788673e1defb5e362d79ae79d18fc0420
hamzaejaz787 <hejaz389@gmail.com>
2023-05-30 19:20:12 +0500

    image carousel and slider added

--- src/containers/MainContent/MainContent.jsx
+++ src/containers/MainContent/MainContent.jsx
@@ -1,12 +1,29 @@
 import ProductImage from "../ProductImage/ProductImage";
 import Product from "../Product/Product";
 
+import imageOne from "../../assets/image-product-1.jpg";
+import imageTwo from "../../assets/image-product-2.jpg";
+import imageThree from "../../assets/image-product-3.jpg";
+import imageFour from "../../assets/image-product-4.jpg";
+
+import productImageThumbnail1 from "../../assets/image-product-1-thumbnail.jpg";
+import productImageThumbnail2 from "../../assets/image-product-2-thumbnail.jpg";
+import productImageThumbnail3 from "../../assets/image-product-3-thumbnail.jpg";
+import productImageThumbnail4 from "../../assets/image-product-4-thumbnail.jpg";
+
 import "./main-content.css";
 
 const MainContent = () => {
+  const images = [imageOne, imageTwo, imageThree, imageFour];
+  const thumbnailImages = [
+    productImageThumbnail1,
+    productImageThumbnail2,
+    productImageThumbnail3,
+    productImageThumbnail4,
+  ];
   return (
     <main className="main">
-      <ProductImage />
+      <ProductImage images={images} thumbnailImages={thumbnailImages} />
       <Product />
     </main>
   );
--- src/containers/ProductImage/ProductImage.jsx
+++ src/containers/ProductImage/ProductImage.jsx
@@ -1,38 +1,61 @@
-import productImage1 from "../../assets/image-product-1.jpg";
-import productImageThumbnail1 from "../../assets/image-product-1-thumbnail.jpg";
-import productImageThumbnail2 from "../../assets/image-product-2-thumbnail.jpg";
-import productImageThumbnail3 from "../../assets/image-product-3-thumbnail.jpg";
-import productImageThumbnail4 from "../../assets/image-product-4-thumbnail.jpg";
+import { useState } from "react";
 
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 import "./product-image.css";
 
-const ProductImage = () => {
+const ProductImage = ({ images, thumbnailImages }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToImage = (index) => {
+    setCurrentImageIndex(index);
+  };
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const goToPrevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="product__image">
       <div className="main__image-container">
-        <img src={productImage1} alt="" className="product__image-main" />
-        <button className="image-nav previous">
+        <img
+          src={images[currentImageIndex]}
+          alt="main image"
+          className="product__image-main"
+        />
+
+        <button className="image-nav previous" onClick={() => goToPrevImage()}>
           <GrFormPrevious size={27} />
         </button>
-        <button className="image-nav next">
+        <button className="image-nav next" onClick={goToNextImage}>
           <GrFormNext size={27} />
         </button>
       </div>
 
       <div className="product__image-thumbnails">
-        <div className="product__image-thumbnail">
-          <img src={productImageThumbnail1} alt="" />
-        </div>
-        <div className="product__image-thumbnail">
-          <img src={productImageThumbnail2} alt="" />
-        </div>
-        <div className="product__image-thumbnail">
-          <img src={productImageThumbnail3} alt="" />
-        </div>
-        <div className="product__image-thumbnail">
-          <img src={productImageThumbnail4} alt="" />
-        </div>
+        {thumbnailImages.map((image, index) => (
+          <div
+            className={
+              index === currentImageIndex
+                ? "product__image-thumbnail active-thumbnail"
+                : "product__image-thumbnail"
+            }
+          >
+            <img
+              key={index}
+              src={image}
+              alt=""
+              onClick={() => goToImage(index)}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
